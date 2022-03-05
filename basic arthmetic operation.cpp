82e4fe7738230ca69b0d69cb6687052bb9ebe69e
Muhammad Hasnain <98162005+mhasnain-hsr@users.noreply.github.com>
2022-03-05 21:11:34 +0500

    Add files via upload

---
+++ basic arthmetic operation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num1=0,num2=0;
+	int a;
+	cout << "\n\n";
+	cout << "Enter the Number: "<<endl;
+	cin>>num1;
+	cout << "5
+	6
+	5Enter the Second Number: "<<endl;
+	cin>>num2;
+	
+	a = num1 * num2;
+	
+	cout<<" \t"<<num1<<" * "<<num2<<" = "<<a;
+	return 0;
+}
